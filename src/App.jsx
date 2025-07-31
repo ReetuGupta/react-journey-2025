b3942aba7ebedb26630dbabdf5c70883e4e36a30
ReetuGupta <gupta.mukn2001@gmail.com>
2025-07-31 21:43:17 +0530

    components in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import './App.css'
-// import MyComponent from './components/Day3'
-import {Header, Footer} from './components/Day3'
-
-function App() {
+export default function App() {
   return (
-      <div>
-        <Header/>
-        <h1>
-          Main
-        </h1>
-        <Footer/>
-      </div>
+  <>
+    <NetflixCard/>
+    <NetflixCard/>
+    <NetflixCard/>
+    <NetflixCard/>
+  </>
   );
 }
 
-export default App
-
-
+const NetflixCard = () => {
+  return (
+    <div>
+      <img src="harry-potter1.jpg" alt="harry-potter1.jpg"/>
+      <h2>Harry Potter and the Philosopher's Stone</h2>
+      <h3>Rating: 8.2</h3>
+      <p>
+        Harry Potter, an eleven-year-old orphan, discovers that he is a wizard
+        and is invited to study at Hogwarts. Even as he escapes a dreary life
+        and enters a world of magic, he finds trouble awaiting him.
+      </p>
+    </div>
+  );
+}
