753230550502c49aeeb60a9945f04e3dcaf5d7fa
ReetuGupta <gupta.mukn2001@gmail.com>
2025-08-01 09:52:51 +0530

    dynamic values in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 export default function App() {
   return (
-  <>
-    <NetflixCard/>
-    <NetflixCard/>
-    <NetflixCard/>
-    <NetflixCard/>
-  </>
+    <>
+      <NetflixCard />
+      <NetflixCard />
+      <NetflixCard />
+      <NetflixCard />
+    </>
   );
 }
 
 const NetflixCard = () => {
+  const name = "Harry Potter and the Philosopher's Stone";
+  const summary =
+    "Harry Potter, an eleven-year-old orphan, discovers that he is a wizard and is invited to study at Hogwarts. Even as he escapes a dreary lif and enters a world of magic, he finds trouble awaiting him.";
+  const seriesGenre = () => {
+    const genre = "Adventure";
+    return genre;
+  }
   return (
     <div>
-      <img src="harry-potter1.jpg" alt="harry-potter1.jpg"/>
-      <h2>Harry Potter and the Philosopher's Stone</h2>
-      <h3>Rating: 8.2</h3>
-      <p>
-        Harry Potter, an eleven-year-old orphan, discovers that he is a wizard
-        and is invited to study at Hogwarts. Even as he escapes a dreary life
-        and enters a world of magic, he finds trouble awaiting him.
-      </p>
+      <img src="harry-potter1.jpg" alt="harry-potter1.jpg" />
+      <h2>Name: {name} </h2> {/*dynamic values in jsx*/}
+      <h3>Rating: {6 + 2.2} </h3> {/*expression*/}
+      <p>Summary: {summary} </p>
+      <p>Genre: {seriesGenre()} </p> {/*function*/}
     </div>
   );
-}
+};
