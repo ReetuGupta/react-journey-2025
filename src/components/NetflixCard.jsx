73748fd9ace84c2863d44fbf44b003ca3d538d80
ReetuGupta <gupta.mukn2001@gmail.com>
2025-08-01 18:48:53 +0530

    looping in jsx

--- src/components/NetflixCard.jsx
+++ src/components/NetflixCard.jsx
@@ -1,21 +1,29 @@
-export const NetflixCard = () => {
-  const name = "Harry Potter and the Philosopher's Stone";
-  const summary =
-    "Harry Potter, an eleven-year-old orphan, discovers that he is a wizard and is invited to study at Hogwarts. Even as he escapes a dreary lif and enters a world of magic, he finds trouble awaiting him.";
-  const seriesGenre = () => {
-    const genre = "Adventure";
-    return genre;
-  }
-  let age = 25;
+import HPData from '../api/HPData.json'
 
+export const NetflixCard = () => {
   return (
     <div>
-      <img src="harry-potter1.jpg" alt="harry-potter1.jpg" />
-      <h2>Name: {name} </h2> {/*dynamic values in jsx*/}
-      <h3>Rating: {6 + 2.2} </h3> {/*expression*/}
-      <p>Summary: {summary} </p>
-      <p>Genre: {seriesGenre()} </p> {/*function*/}
-      <button> {(age>=18) ? "Watch Now" : "Not Available"} </button>
+      {HPData.map((currentElem) => {
+        return(
+          <div key={currentElem.id}>
+          <img 
+            src= {currentElem.img_url} 
+            alt={currentElem.name} 
+            width="40%" 
+            height= "40%" 
+          />
+          <h2>Name: {currentElem.name} </h2> 
+          <h3>Rating: {currentElem.rating} </h3> 
+          <p>Year: {currentElem.year} </p>
+          <p>Summary: {currentElem.description} </p>
+          <p>Genre: {currentElem.genre} </p> 
+          <a href= {currentElem.watch_url} target="_blank">
+            <button> Watch Now </button>
+          </a>
+          </div>
+        )
+        })
+      }
     </div>
   );
 };
